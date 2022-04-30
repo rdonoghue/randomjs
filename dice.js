7d6ffddb1a1fff5eb5337169f3ba89d9ffed9418
BuildTools <unconfigured@null.spigotmc.org>
2022-04-29 20:12:56 -0400

    2d6

--- dice.js
+++ dice.js
@@ -6,19 +6,28 @@ var dice = {
   }
 }
 
-
+var twodice = {
+  sides: 6,
+  roll: function () {
+    var randomNumber1 = Math.floor(Math.random() * this.sides) + 1;
+    var randomNumber2 = Math.floor(Math.random() * this.sides) + 1;
+    var dicesum = randomNumber1 + randomNumber2;
+    return dicesum;
+  }
+}
 
 //Prints dice roll to the page
 
 function printNumber(number) {
   var placeholder = document.getElementById('placeholder');
   placeholder.innerHTML = number;
+
 }
 
 var button = document.getElementById('button');
 
-button.onclick = function() {
-  var result = dice.roll();
+button.onclick = function () {
+  var result = twodice.roll();
   printNumber(result);
 };
 
